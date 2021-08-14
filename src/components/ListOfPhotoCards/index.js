a5145dada93ef1df2acdcacf581c4cf8005fea1e
MrManu <superbrandon2018@gmail.com>
2021-08-13 20:29:31 -0500

    PhotoCards

---
+++ src/components/ListOfPhotoCards/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { PhotoCard } from "../PhotoCard";
+
+export const ListOfPhotoCards = () => {
+    return(
+        <ul>
+            {
+                [1,2,3].map(id => {
+                    return(
+                        <PhotoCard key={id}></PhotoCard>
+                    )
+                })
+            }
+        </ul>
+    )
+}
